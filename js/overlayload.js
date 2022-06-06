09e27589cd3deebd209fe439176a0f190a2221a5
TotheMoon12 <46846455+TotheMoon12@users.noreply.github.com>
2022-06-06 23:29:02 +0900

    Add files via upload

---
+++ js/overlayload.js
@@ -0,0 +1,17 @@
+function open_url(type, url, data, target) {
+	$('#exp-over').css("display", "block");
+	$.ajax({
+		type: type,
+		url: url,
+		data: data,
+		dataType:"jsonp",
+		success: function(data) {
+			$("#" + target).html(data);
+		}
+	})
+}
+
+function myload(url) {	
+	$("#exp-over").load(url);
+	$('#exp-over').css("display", "block");
+}

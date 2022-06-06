0adb690a22f73f6bd0d767d1f7ab329b67c107fa
TotheMoon12 <46846455+TotheMoon12@users.noreply.github.com>
2022-06-06 23:29:21 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,6 @@
-function open_url(type, url, data, target) {
-	$('#exp-over').css("display", "block");
-	$.ajax({
-		type: type,
-		url: url,
-		data: data,
-		dataType:"jsonp",
-		success: function(data) {
-			$("#" + target).html(data);
-		}
-	})
-}
-
-function myload(url) {	
-	$("#exp-over").load(url);
-	$('#exp-over').css("display", "block");
-}
 ;(function () {
 	
-	'use strict';
-
-	var toggleOverlay = function() {
-
-	};
-
-	
+	'use strict';	
 
 	var isMobile = {
 		Android: function() {

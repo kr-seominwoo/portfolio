f0c1439775c25518f87c8b7f9a454a208424aadd
DESKTOP-24FN8J6\seo <tjalsdnss@gmail.com>
2022-06-08 14:54:00 +0900

    url 이동 테스트

--- js/overlayload.js
+++ js/overlayload.js
@@ -11,11 +11,36 @@ function open_url(type, url, data, target) {
 	})
 }
 
+
 function myload(url) {	
-	$("#exp-over").load(url);
-	$("#exp-over").css("display", "block");
+	console.log(url);
+	let curURL = $(window.location)[0].href;
+	let lastIndex = curURL.lastIndexOf("/");
+	let loadURL = curURL.substring(0, lastIndex + 1) + url;
+	console.log(loadURL);
+	$(".project-overlay").load(url);
+	$(".project-overlay").css("display", "block");
+	window.history.pushState('', '', loadURL);
 }
 
 function closeOverlay() {
-	$("#exp-over").css("display", "none");
+	$(".project-overlay").css("display", "none");
 }
+
+// $("button").on("click", "#test", function(event) {
+// 	console.log("hello");
+// 	let url = "project-overlay.html";
+// 	let curURL = $(window.location)[0].href;
+// 	let lastIndex = curURL.lastIndexOf("/");
+// 	let loadURL = curURL.substring(0, lastIndex + 1) + url;
+// 	window.history.pushState(null, null, loadURL);
+// })
+
+$("button[id='test']").click(function() {
+	console.log("hello");
+	let url = "project-overlay.html";
+	let curURL = $(window.location)[0].href;
+	let lastIndex = curURL.lastIndexOf("/");
+	let loadURL = curURL.substring(0, lastIndex + 1) + url;
+	window.history.pushState(null, null, url);
+})

8a66369b5a774a6246b29ed45ce4edd2ed8caf94
DESKTOP-24FN8J6\seo <tjalsdnss@gmail.com>
2022-06-21 14:39:05 +0900

    주석 추가

--- js/overlayload.js
+++ js/overlayload.js
@@ -1,3 +1,4 @@
+/* animation */
 var contentWayPoint = function() {
 	var i = 0;
 	$('.animate-box').waypoint( function( direction ) {
@@ -69,12 +70,15 @@ var navigationSection = function() {
 };
 
 function myload(url) {	
+	/* project-overlay의 자식들에 애니메이션 추가 */
 	$(".project-overlay").load(url, contentWayPoint);
+
 	$(".project-overlay").removeClass("hidden");
 	$("#colorlib-page").addClass("hidden");
+
+	/* 최상단으로 스크롤 이동 */
 	$('html').animate({scrollTop: 0}, 10);
 }
-
 
 function closeOverlay() {
 	let curURL = $(window.location)[0].href;
@@ -85,6 +89,8 @@ function closeOverlay() {
 	$(".project-overlay").addClass("hidden");
 	$(".project-overlay").empty();
 	$("#colorlib-page").removeClass("hidden");
+
+	/* 메뉴 네비게이션 활성화 */
 	navigationSection();
 	$('html').animate({scrollTop: $('[data-section="project"]').offset().top - 55}, 10);
 }
